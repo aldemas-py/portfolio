0e9ee2d01fd8385a3b508d7235c8d6084eb175f0
aldemas-py <njengasam2406@gmail.com>
2022-01-24 12:51:11 +0300

    crude code

--- js/ext.js
+++ js/ext.js
@@ -1,28 +1,49 @@
-$(document).ready(function () {
-    $('#main').hide();
-    $('#main').delay(1000).fadeIn();
+// initial page
+// $(document).ready(function () {
+//     $('#main').hide();
+//     $('#main').delay(1000).fadeIn();
 
-    function moveDiv() {
+//     // centering div
+//     function moveDiv() {
 
-        // $('#main').fadeTo('opacity', 0.9);
-        windowWidth = $(window).width();
-        windowHeight = $(window).height();
+//         // $('#main').fadeTo('opacity', 0.9);
+//         windowWidth = $(window).width();
+//         windowHeight = $(window).height();
 
-        objWidth = $('#center').width();
-        objHeight = $('#center').height();
+//         objWidth = $('#center').width();
+//         objHeight = $('#center').height();
 
-        mainHeight = windowHeight*0.95;
-        mainWidth = windowWidth*0.985;
-    //   alert(mainHeight)
-        $('#main').height(mainHeight+'px');
-        $('#main').width(mainWidth+'px');
-        $('#center').css('top', (windowHeight - objHeight)/2).css('left', (windowWidth - objWidth)/2);
+//         mainHeight = windowHeight*0.95;
+//         mainWidth = windowWidth*0.985;
+//     //   alert(mainHeight)
+//         $('#main').height(mainHeight+'px');
+//         $('#main').width(mainWidth+'px');
+//         $('#center').css('top', (windowHeight - objHeight)/2).css('left', (windowWidth - objWidth)/2);
       
       
-    }
-    moveDiv();
+//     }
+//     moveDiv();
   
-    $(window).resize(function () { 
-      moveDiv();
-    });
+//     $(window).resize(function () { 
+//       moveDiv();
+//     });
+//   });
+
+// actual page
+
+function footerAlign() {
+    $('#footer').css('display', 'block');
+    $('#footer').css('height', 'auto');
+    var footerHeight = $('#footer').outerHeight();
+    $('body').css('padding-bottom', footerHeight);
+    $('#footer').css('height', footerHeight);
+  }
+  
+  
+  $(document).ready(function(){
+    footerAlign();
+  });
+  
+  $( window ).resize(function() {
+    footerAlign();
   });
