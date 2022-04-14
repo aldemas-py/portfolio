d5a6df7941b3a1741dbfb1617a1c61baa9539ee8
aldemas-py <njengasam2406@gmail.com>
2022-04-14 04:08:44 +0300

    first live page

--- js/ext.js
+++ js/ext.js
@@ -1,3 +1,11 @@
+const nav = document.querySelector(".navbar");
+fetch("head/header.html")
+  .then((res) => res.text())
+  .then((data) => {
+    nav.innerHTML = data;
+  });
+
+
 function footerAlign() {
   $("#footer").css("display", "block");
   $("#footer").css("height", "auto");
@@ -8,8 +16,19 @@ function footerAlign() {
 
 $(document).ready(function () {
   footerAlign();
+  $("footer").html(htmlString);
+
+
 });
 
 $(window).resize(function () {
   footerAlign();
+
 });
+
+const foot = document.querySelector(".footer");
+fetch("head/footer.html")
+  .then((res) => res.text())
+  .then((data) => {
+    foot.innerHTML = data;
+  });
