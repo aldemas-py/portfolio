3a31cd2af6372dcee41e5295be0095ccc60403db
aldemas-py <njengasam2406@gmail.com>
2022-04-29 14:50:03 +0300

    new

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=900px, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Njenga Sam</title>
+</head>
+<body>
+    <div id="body">
+        <div id="top"> this is the nav bar</div>
+        <div id="content">
+            <div class="sec1">Designer, FullStack developer & Mentor </div>
+            <div class="sec2">I design and code websites. I love what I do.</div>
+            <div class="me">
+                <img class="myImg" src="images/me.jpg" alt="My image">
+            </div>
+            <div class="sec3">
+                <p id="hhh">Hi, I'm Sam.Nice to meet you.
+                    </p> Since beginning my journey as a freelance designer nearly 4 years ago, I've done remote work startups and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
+                    </p>
+            </div>
+            <div class="sec4">
+                <div class="designer">
+                    <p>Designer</p>
+                    <p>I  value simple content structure, clean designs and thoutful interactions</p>
+                    <br><br>
+                    <p>Things I enjoy designing</p>
+                    <br><br>
+                    <p>UX,UI,Websites and mobile apps</p>
+                    <br><ol>
+                    <li>Design tools:</li>
+                    <li>.</li>
+                        <li>Adobe Photoshop</li>
+                        <li>Figma</li>
+                        <li>Sketch</li>
+                        <li>Adobe Illustrator </li>
+                        <li>Corel Draw</li>
+                        <li></li>
+                        <li></li>
+                    </ol>
+                </div>
+                <div class="fullStack">
+                    <p>FullStack</p>
+                    <p>I  like to code from scratch and enjoy bringing my designs and ideas to life</p>
+                    <br><br>
+                    <p>Languages I speak:</p>
+                    <br><br>
+                    <p>HTML, CSS, JavaSript, PHP, Python, SQL, MySQL, C, C++, JAVA</p>
+                    <br>
+                    <p>Dev tools:</p>
+                    <ol>
+                        <li>VS Code</li>
+                        <li>Bootstrap</li>
+                        <li>Kaggle</li>
+                        <li>Github </li>
+                        <li>Gitlab</li>
+                        <li>Terminal</li>
+                        <li>Android Studio</li>
+                        <li>XAMMP</li>
+                    </ol>
+                </div>
+                <div class="mentor">
+                    <p>Mentor</p>
+                    <p>I  love and enjoy working with fellow Developers to bring Ideas to life and improve their craft.</p>
+                    <br><br>
+                    <p>Experience I draw from:</p>
+                    <br><br>
+                    <p>UX/UI, Product design and Freelancing</p>
+                    <br>
+                    <p>Design tools:</p>
+                    <ol>
+                        <li>Adobe Photoshop</li>
+                        <li>Figma</li>
+                        <li>Sketch</li>
+                        <li>Adobe Illustrator </li>
+                        <li>Corel Draw</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <div id="footer">footer</div>
+   
+    </div>
+    <script src="js/jquery.js"></script>
+    <script src='js/ext.js'></script>
+</body>
+</html>
