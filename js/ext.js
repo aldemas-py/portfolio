609c0fd7a8d5de21bb7af33246633b9921ed0a5d
Aldemas <njengasam2406@gmail.com>
2024-01-04 12:17:04 -0800

    code refactored

--- js/ext.js
+++ js/ext.js
@@ -5,7 +5,25 @@ fetch("head/header.html")
   .then((data) => {
     nav.innerHTML = data;
   });
+// html date
+function setDynamicWebDate(elementId, startDateString) {
+  const startDate = new Date(startDateString);
 
+  function updateWebDate() {
+    const currentDate = new Date();
+    const timeDifference = currentDate - startDate;
+
+    const years = Math.floor(timeDifference / (1000 * 60 * 60 * 24 * 365));
+
+    document.getElementById(elementId).innerHTML = `${years} ${years === 1 ? 'year' : 'years'}`;
+  }
+
+  updateWebDate();
+
+  setInterval(updateWebDate, 24 * 60 * 60 * 1000); // Update every 24 hours
+}
+
+  
 // footer
 
 const foot = document.querySelector(".footer");
