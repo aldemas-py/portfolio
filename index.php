fa47ac3d908e1ad9c8f7106a742d3f6af463fafc
aldemas-py <njengasam2406@gmail.com>
2025-11-23 15:10:34 -0600

    revamped the code

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="css/style.css">
     <title>Njenga Sam</title>
 </head>
+
 <body>
     <div id="body">
         <div id="top"> this is the nav bar</div>
@@ -18,20 +20,24 @@
             </div>
             <div class="sec3">
                 <p id="hhh">Hi, I'm Sam.Nice to meet you.
-                    </p> Since beginning my journey as a freelance designer nearly 4 years ago, I've done remote work startups and collaborated with talented people to create digital products for both business and consumer use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design problem at a time.
-                    </p>
+                </p> Since beginning my journey as a freelance designer nearly 4 years ago, I've done remote work
+                startups and collaborated with talented people to create digital products for both business and consumer
+                use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design
+                problem at a time.
+                </p>
             </div>
             <div class="sec4">
                 <div class="designer">
                     <p>Designer</p>
-                    <p>I  value simple content structure, clean designs and thoutful interactions</p>
+                    <p>I value simple content structure, clean designs and thoutful interactions</p>
                     <br><br>
                     <p>Things I enjoy designing</p>
                     <br><br>
                     <p>UX,UI,Websites and mobile apps</p>
-                    <br><ol>
-                    <li>Design tools:</li>
-                    <li>.</li>
+                    <br>
+                    <ol>
+                        <li>Design tools:</li>
+                        <li>.</li>
                         <li>Adobe Photoshop</li>
                         <li>Figma</li>
                         <li>Sketch</li>
@@ -43,7 +49,7 @@
                 </div>
                 <div class="fullStack">
                     <p>FullStack</p>
-                    <p>I  like to code from scratch and enjoy bringing my designs and ideas to life</p>
+                    <p>I like to code from scratch and enjoy bringing my designs and ideas to life</p>
                     <br><br>
                     <p>Languages I speak:</p>
                     <br><br>
@@ -63,7 +69,8 @@
                 </div>
                 <div class="mentor">
                     <p>Mentor</p>
-                    <p>I  love and enjoy working with fellow Developers to bring Ideas to life and improve their craft.</p>
+                    <p>I love and enjoy working with fellow Developers to bring Ideas to life and improve their craft.
+                    </p>
                     <br><br>
                     <p>Experience I draw from:</p>
                     <br><br>
@@ -81,9 +88,10 @@
             </div>
         </div>
         <div id="footer">footer</div>
-   
+
     </div>
     <script src="js/jquery.js"></script>
     <script src='js/ext.js'></script>
 </body>
+
 </html>
