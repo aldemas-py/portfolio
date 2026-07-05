31095c393d5f1c44cb5680c37a0518343fc31f60
aldemas-py <55619707+aldemas-py@users.noreply.github.com>
2026-07-05 14:04:29 -0700

    web stayling started

--- index.php
+++ index.php
@@ -4,94 +4,81 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=900px, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/main.css">
     <title>Njenga Sam</title>
 </head>
 
 <body>
     <div id="body">
-        <div id="top"> this is the nav bar</div>
-        <div id="content">
-            <div class="sec1">Designer, FullStack developer & Mentor </div>
-            <div class="sec2">I design and code websites. I love what I do.</div>
+        <header id="top">This is the nav bar</header>
+        <main id="content">
+            <h1 class="sec1">Designer, FullStack developer & Mentor</h1>
+            <p class="sec2">I design and code websites. I love what I do.</p>
             <div class="me">
-                <img class="myImg" src="images/me.jpg" alt="My image">
+                <img class="myImg" src="images/me.jpg" alt="Portrait of Kabura Njenga">
             </div>
-            <div class="sec3">
-                <p id="hhh">Hi, I'm Sam.Nice to meet you.
-                </p> Since beginning my journey as a freelance designer nearly 4 years ago, I've done remote work
-                startups and collaborated with talented people to create digital products for both business and consumer
-                use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design
-                problem at a time.
+            <section class="sec3">
+                <p id="hhh">Hi, I'm Sam. Nice to meet you.</p>
+                <p>
+                    Since beginning my journey as a freelance designer nearly 4 years ago, I've done remote work
+                    startups and collaborated with talented people to create digital products for both business and consumer
+                    use. I'm quietly confident, naturally curious, and perpetually working on improving my chops one design
+                    problem at a time.
                 </p>
-            </div>
-            <div class="sec4">
+            </section>
+            <section class="sec4">
                 <div class="designer">
                     <p>Designer</p>
-                    <p>I value simple content structure, clean designs and thoutful interactions</p>
-                    <br><br>
-                    <p>Things I enjoy designing</p>
-                    <br><br>
-                    <p>UX,UI,Websites and mobile apps</p>
-                    <br>
+                    <p>I value simple content structure, clean designs, and thoughtful interactions.</p>
+                    <p>Things I enjoy designing:</p>
+                    <p>UX, UI, websites, and mobile apps</p>
                     <ol>
                         <li>Design tools:</li>
-                        <li>.</li>
                         <li>Adobe Photoshop</li>
                         <li>Figma</li>
                         <li>Sketch</li>
-                        <li>Adobe Illustrator </li>
+                        <li>Adobe Illustrator</li>
                         <li>Corel Draw</li>
-                        <li></li>
-                        <li></li>
                     </ol>
                 </div>
                 <div class="fullStack">
                     <p>FullStack</p>
-                    <p>I like to code from scratch and enjoy bringing my designs and ideas to life</p>
-                    <br><br>
+                    <p>I like to code from scratch and enjoy bringing my designs and ideas to life.</p>
                     <p>Languages I speak:</p>
-                    <br><br>
-                    <p>HTML, CSS, JavaSript, PHP, Python, SQL, MySQL, C, C++, JAVA</p>
-                    <br>
+                    <p>HTML, CSS, JavaScript, PHP, Python, SQL, MySQL, C, C++, Java</p>
                     <p>Dev tools:</p>
                     <ol>
                         <li>VS Code</li>
                         <li>Bootstrap</li>
                         <li>Kaggle</li>
-                        <li>Github </li>
-                        <li>Gitlab</li>
+                        <li>GitHub</li>
+                        <li>GitLab</li>
                         <li>Terminal</li>
                         <li>Android Studio</li>
-                        <li>XAMMP</li>
+                        <li>XAMPP</li>
                     </ol>
                 </div>
                 <div class="mentor">
                     <p>Mentor</p>
-                    <p>I love and enjoy working with fellow Developers to bring Ideas to life and improve their craft.
-                    </p>
-                    <br><br>
+                    <p>I love and enjoy working with fellow developers to bring ideas to life and improve their craft.</p>
                     <p>Experience I draw from:</p>
-                    <br><br>
-                    <p>UX/UI, Product design and Freelancing</p>
-                    <br>
+                    <p>UX/UI, product design, and freelancing</p>
                     <p>Design tools:</p>
                     <ol>
                         <li>Adobe Photoshop</li>
                         <li>Figma</li>
                         <li>Sketch</li>
-                        <li>Adobe Illustrator </li>
+                        <li>Adobe Illustrator</li>
                         <li>Corel Draw</li>
                     </ol>
                 </div>
-            </div>
-        </div>
-        <div id="footer">footer</div>
+            </section>
+        </main>
+        <footer id="footer">footer</footer>
 
     </div>
-    <script src="js/jquery.js"></script>
-    <script src='js/ext.js'></script>
+    <script src="js/ext.js"></script>
 </body>
 
 </html>
